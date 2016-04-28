04be71f5cf8fd4d3eebc3fefc7eb941f4d017436
Oleg Pylypchak <liole.plo@gmail.com>
2016-04-28 08:38:47 +0300

    Test & question CRUD

---
+++ ITS/Controllers/QuestionController.cs
@@ -0,0 +1,119 @@
+﻿using ITS.Domain.Entities;
+using ITS.Domain.UnitOfWork.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITS.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private IUnitOfWork unitOfWork;
+
+		public QuestionController(IUnitOfWork uow)
+		{
+			this.unitOfWork = uow;
+		}
+
+		public ActionResult Edit(int id)
+		{
+			var question = unitOfWork.Questions.GetByID(id);
+			ViewBag.Create = false;
+			if (question is ABCDQuestion)
+			{
+				ViewBag.ABCDList = selectABCDList((question as ABCDQuestion).Answer);
+				return View("ABCDEditor", question);
+			}
+			if (question is TextQuestion)
+			{
+				return View("TextEditor", question);
+			}
+			throw new InvalidOperationException();
+		}
+
+		public ActionResult CreateABCD(int id)
+		{
+			var question = new ABCDQuestion()
+			{
+				TestID = id
+			};
+			ViewBag.Create = true;
+			ViewBag.ABCDList = selectABCDList();
+			return View("ABCDEditor", question);
+		}
+		public ActionResult CreateText(int id)
+		{
+			var question = new TextQuestion()
+			{
+				TestID = id
+			};
+			ViewBag.Create = true;
+			return View("TextEditor", question);
+		}
+
+		[HttpPost]
+		public ActionResult EditABCD(ABCDQuestion question)
+		{
+			return Edit(question);
+		}
+		[HttpPost]
+		public ActionResult EditText(TextQuestion question)
+		{
+			return Edit(question);
+		}
+
+		[HttpPost]
+		public ActionResult CreateABCD(ABCDQuestion question)
+		{
+			return Create(question);
+		}
+		[HttpPost]
+		public ActionResult CreateText(TextQuestion question)
+		{
+			return Create(question);
+		}
+
+
+		[HttpPost]
+		public ActionResult Edit(Question question)
+		{
+			unitOfWork.Questions.Update(question);
+			unitOfWork.Save();
+			return RedirectToAction("Details", "Test", new { id = question.TestID });
+		}
+
+		[HttpPost]
+		public ActionResult Create(Question question)
+		{
+			unitOfWork.Questions.Insert(question);
+			unitOfWork.Save();
+			return RedirectToAction("Details", "Test", new { id = question.TestID });
+		}
+
+		public ActionResult Delete(int id)
+		{
+			var question = unitOfWork.Questions.GetByID(id);
+			var testID = question.TestID;
+			unitOfWork.Questions.Delete(question);
+			unitOfWork.Save();
+			return RedirectToAction("Details", "Test", new { id = testID });
+		}
+
+		private List<SelectListItem> selectABCDList(ABCDAnswer selected = ABCDAnswer.A)
+		{
+			var list = new List<SelectListItem>();
+			for (var i = 0; i < 4; ++i)
+			{
+				list.Add(new SelectListItem() { 
+					Text = ((char)('А' + i)).ToString(),
+					Value = i.ToString(),
+					Selected = (int)selected == i
+				});
+			}
+			return list;
+		}
+
+    }
+}
---
+++ ITS/Controllers/TestController.cs
@@ -0,0 +1,89 @@
+﻿using ITS.Domain.Entities;
+using ITS.Domain.UnitOfWork.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITS.Controllers
+{
+    public class TestController : Controller
+    {
+		private IUnitOfWork unitOfWork;
+
+		public TestController(IUnitOfWork uow)
+		{
+			this.unitOfWork = uow;
+		}
+ 
+        public ActionResult List()
+        {
+			var tests = CurrentUser().Tests;
+            return View(tests);
+        }
+
+		public ActionResult Details(int id)
+		{
+			var test = unitOfWork.Tests.GetByID(id);
+			return View(test);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			var test = unitOfWork.Tests.GetByID(id);
+			var subjects = unitOfWork.Subjects.GetAll().Select(s => new SelectListItem()
+			{
+				Text = s.Name,
+				Value = s.ID.ToString(),
+				Selected = s.ID == test.SubjectID
+			});
+			ViewBag.Create = false;
+			ViewBag.Subjects = subjects;
+			return View(test);
+		}
+		[HttpPost]
+		public ActionResult Edit(Test test)
+		{
+			unitOfWork.Tests.Update(test);
+			unitOfWork.Save();
+			return RedirectToAction("Details", new { id = test.ID });
+		}
+
+		public ActionResult Create()
+		{
+			var test = new Test()
+			{
+				UserID = CurrentUser().ID
+			};
+			var subjects = unitOfWork.Subjects.GetAll().Select(s => new SelectListItem()
+			{
+				Text = s.Name,
+				Value = s.ID.ToString()
+			}); 
+			ViewBag.Create = true;
+			ViewBag.Subjects = subjects;
+			return View("Edit", test);
+		}
+		[HttpPost]
+		public ActionResult Create(Test test)
+		{
+			unitOfWork.Tests.Insert(test);
+			unitOfWork.Save();
+			return RedirectToAction("Details", new { id = test.ID });
+		}
+
+		public ActionResult Delete(int id)
+		{
+			unitOfWork.Tests.Delete(id);
+			unitOfWork.Save();
+			return RedirectToAction("List");
+		}
+
+		private User CurrentUser()
+		{
+			return unitOfWork.Users.GetByID(1);
+		}
+
+    }
+}
